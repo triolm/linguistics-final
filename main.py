b42d922bbb93fead7e0fd9b89b9263cb43db0829
Marisa Triola <marisatriola@gmail.com>
2023-12-13 14:23:13 -0500

    add generation based on IPA

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from ngram import NGram
+import eng_to_ipa as ipa
 
 text = None;
 with open('./data/sherlock.txt', 'r') as file:
     text = file.read()
+
+    # text = text[1 : 200000]
+    # text = ipa.convert(text);
 
 def getNgram(ngrams, str):
     ngram = ngrams.get(str)
@@ -14,19 +18,25 @@ def getNgram(ngrams, str):
 def getNRgams(text, n):
     ngrams = {}
     for i in range(len(text) - (n-1)):
-        ngram = getNgram(ngrams, text[i: i+(n-1)])
-        ngram.add(text[i + (n-1)])
+        substr = text[i: i+(n-1)]
+        next = text[i + (n-1)]
+        ngram = getNgram(ngrams, substr)
+        ngram.add(next)
     return ngrams
 
 def generate(ngrams, n, characters):
-    text = "the quick fox jumps over the lazy dog"[0:n-1]
+    built = text[0:n-1]
     for i in range(characters):
         if(1 % 10 == 0):
-            print(text)
-        substr = text[len(text)-(n-1):len(text)]
-        text += ngrams[substr].getRand()
+            print(built)
+        substr = built[len(built)-(n-1):len(built)]
+        # rand = "e"
+        # while( "e" in rand):
+        rand = ngrams[substr].getRand();
+        
+        built += rand
 
-    return text
+    return built
 
-n = 6
-print(generate(getNRgams(text, n),n, 500))
+n = 3
+print(generate(getNRgams(text, n),n, 2000))
